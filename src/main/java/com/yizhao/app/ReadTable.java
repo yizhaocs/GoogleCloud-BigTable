c17cb62b1129a57cb022d165f6c52b8a805a919f
yzhao <yi.zhao@adara.com>
2017-07-24 14:29:58 -0700

    1

--- src/main/java/com/yizhao/app/ReadTable.java
+++ src/main/java/com/yizhao/app/ReadTable.java
@@ -14,6 +14,22 @@ import java.util.Map;
  * Created by yzhao on 7/17/17.
  */
 public class ReadTable {
+    public static void executeReadingArowByItsKeyMap(Table table, byte[] rowkey, byte[] COLUMN_FAMILY_NAME, byte[] COLUMN_NAME) throws Exception{
+        long startTime = System.nanoTime();
+        Result getResult = table.get(new Get(rowkey));
+        byte[] valueBytes = getResult.getValue(COLUMN_FAMILY_NAME, COLUMN_NAME);
+        ByteArrayInputStream byteIn = new ByteArrayInputStream(valueBytes);
+        ObjectInputStream in = new ObjectInputStream(byteIn);
+        Map<Integer, String> data2 = (Map<Integer, String>) in.readObject();
+
+        System.out.println("valueBytes:" + data2);
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime)/1000000; // in milliseconds
+        System.out.println("total time used for reading one bye one:" + duration + " milliseconds");
+
+    }
+
     public static void executeReadingArowByItsKey(Table table, byte[] rowkey, byte[] COLUMN_FAMILY_NAME, byte[] COLUMN_NAME) throws Exception{
         long startTime = System.nanoTime();
         Result getResult = table.get(new Get(rowkey));
--- src/main/java/com/yizhao/app/TestingMain.java
+++ src/main/java/com/yizhao/app/TestingMain.java
@@ -1,11 +1,13 @@
 package com.yizhao.app;
 
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,17 +30,52 @@ public class TestingMain {
     static Map<String, Map<Integer,KeyValueTs>> map = new HashMap<String, Map<Integer, KeyValueTs>>();
     static int totalIteration = 1000;
     static byte[] columnFaimilyName = "columnFaimilyName".getBytes();
-    static String bigtableTableName = "table14";
+    static String bigtableTableName = "table15";
     static byte[] rowKey = "rowKey".getBytes();
 
     public static void main(String[] args) throws Exception{
-        writeToBigTable( );
-        readBigTable();
+        ProcessCkvData.readThenWrite(map, "src/main/resources/20170712-004428.ps101-lax1.0000000000010309020.csv");
+        System.out.println(map.size());
+
+        //writeToBigTableInt();
+        //readBigTableInt();
+
+        writeToBigTableCKVmap( );
+        readBigTableMap();
     }
 
 
 
-    public static void writeToBigTable( ){
+    public static void writeToBigTableCKVmap( ){
+        try {
+            byte[] tableName = Bytes.toBytes(bigtableTableName);
+
+            Connection connection = BigTableConnection.getConnection();
+            //System.out.println(BigtableHelloWorld.create(connection));
+            System.out.println(CreateTable.execute(connection, tableName, columnFaimilyName));
+            Table table = connection.getTable(TableName.valueOf(tableName));
+
+            long startTime = System.nanoTime();
+
+            int count = 0;
+            for(String cookieId : map.keySet()){
+                byte[] columnQualifier = cookieId.getBytes();
+                Map<Integer, KeyValueTs > ckvMap = map.get(cookieId);
+                ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
+                ObjectOutputStream out = new ObjectOutputStream(byteOut);
+                out.writeObject(ckvMap);
+                InsertTable.execute(table, rowKey, columnFaimilyName, columnQualifier, byteOut.toByteArray());
+            }
+            long endTime = System.nanoTime();
+
+            long duration = (endTime - startTime)/1000000; // in milliseconds
+            System.out.println("total time used for writing:" + duration + " milliseconds ,with count:" + count);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void writeToBigTableInt( ){
         try {
             byte[] tableName = Bytes.toBytes(bigtableTableName);
 
@@ -64,7 +101,7 @@ public class TestingMain {
         }
     }
 
-    public static void readBigTable() throws Exception{
+    public static void readBigTableInt() throws Exception{
         Connection connection = BigTableConnection.getConnection();
         byte[] tableName = Bytes.toBytes(bigtableTableName);
         byte[] rowKey = "rowKey".getBytes();
@@ -76,6 +113,30 @@ public class TestingMain {
              ReadTable.executeReadingArowByItsKey(table, rowKey, columnFaimilyName, columnQualifier);
             // Parse byte array to Map
            // ByteArrayInputStream byteIn = new ByteArrayInputStream(result);
+            //ObjectInputStream in = new ObjectInputStream(byteIn);
+            //Map<Integer, String> data2 = (Map<Integer, String>) in.readObject();
+            //System.out.println("cookie:" + cookieId + " ,ckvMap:" + data2.toString());
+            count ++;
+            //System.out.println(count);
+        }
+        long endTime = System.nanoTime();
+
+        long duration = (endTime - startTime)/1000000; // in milliseconds
+        System.out.println("total time used for reading:" + duration + " milliseconds ,with count:" + count);
+    }
+
+    public static void readBigTableMap() throws Exception{
+        Connection connection = BigTableConnection.getConnection();
+        byte[] tableName = Bytes.toBytes(bigtableTableName);
+        byte[] rowKey = "rowKey".getBytes();
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        int count = 0;
+        long startTime = System.nanoTime();
+        for(String cookieId : map.keySet()) {
+            byte[] columnQualifier = cookieId.getBytes();
+            ReadTable.executeReadingArowByItsKey(table, rowKey, columnFaimilyName, columnQualifier);
+            // Parse byte array to Map
+            // ByteArrayInputStream byteIn = new ByteArrayInputStream(result);
             //ObjectInputStream in = new ObjectInputStream(byteIn);
             //Map<Integer, String> data2 = (Map<Integer, String>) in.readObject();
             //System.out.println("cookie:" + cookieId + " ,ckvMap:" + data2.toString());
