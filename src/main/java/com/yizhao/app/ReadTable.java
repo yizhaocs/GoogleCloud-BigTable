a14ff35a9a9c5f0921ebd10a1e23946006ab287f
yzhao <yi.zhao@adara.com>
2017-07-24 14:14:10 -0700

    1

--- src/main/java/com/yizhao/app/ReadTable.java
+++ src/main/java/com/yizhao/app/ReadTable.java
@@ -15,9 +15,6 @@ import java.util.Map;
  */
 public class ReadTable {
     public static void executeReadingArowByItsKey(Table table, byte[] rowkey, byte[] COLUMN_FAMILY_NAME, byte[] COLUMN_NAME) throws Exception{
-        //Scan scan = new Scan();
-        // StringBuilder result = new StringBuilder();
-        //ResultScanner scanner = table.getScanner(scan);
         long startTime = System.nanoTime();
         Result getResult = table.get(new Get(rowkey));
         byte[] valueBytes = getResult.getValue(COLUMN_FAMILY_NAME, COLUMN_NAME);
@@ -26,17 +23,7 @@ public class ReadTable {
 
         long duration = (endTime - startTime)/1000000; // in milliseconds
         System.out.println("total time used for reading one bye one:" + duration + " milliseconds");
-/*
-        ByteArrayInputStream byteIn = new ByteArrayInputStream(getResult.getValue(COLUMN_FAMILY_NAME, COLUMN_NAME));
-        ObjectInputStream in = new ObjectInputStream(byteIn);
-        Map<Integer, String> data2 = (Map<Integer, String>) in.readObject();
-        */
-        //System.out.println( " ,ckvMap:" + data2.toString());
-        //  System.out.println(Bytes.toString(getResult.getValue(COLUMN_FAMILY_NAME, COLUMN_NAME)));
-       /* byte[] valueBytes = null;
-        for (Result row : scanner) {
-            valueBytes = row.getValue(COLUMN_FAMILY_NAME, COLUMN_NAME);
-        }*/
+
     }
 
 
